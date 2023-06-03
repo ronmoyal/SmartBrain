da0213839769e33b8e8849799dc15afdc1c2b87f
ronmoyal <ronmoyal1902@gmail.com>
2023-06-04 00:16:11 +0300

    front - start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+    render() {
+      return (
+      <div className="App">
+        <Navigation/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Rank = ()=> {
+  return (
+    <div>
+      
+      <div className='white f3'>
+        {`Ron, your current entry count is...`}
+      </div>
+
+      <div className='white f2'>
+        {'#5'}
+      </div>
+
+    </div>
+  );
+  }
+
+export default Rank;
